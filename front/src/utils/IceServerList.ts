49c165215cb353e35c416ee9e19cc96028af7d9d
Hongkwon Lee <honlee@c7r5s6.42seoul.kr>
2021-09-06 10:15:54 +0900

    [hotfix] iceTransportPolicy 다시 all 로 원상복귀 (default)

--- front/src/utils/IceServerList.ts
+++ front/src/utils/IceServerList.ts
@@ -16,5 +16,5 @@ export const iceConfig: RTCConfiguration = {
       username: 'webrtc@live.com',
     },
   ],
-  iceTransportPolicy: 'relay', //참조 : https://developer.mozilla.org/en-US/docs/Web/API/RTCConfiguration/iceTransportPolicy
+  iceTransportPolicy: 'all', //참조 : https://developer.mozilla.org/en-US/docs/Web/API/RTCConfiguration/iceTransportPolicy
 };
