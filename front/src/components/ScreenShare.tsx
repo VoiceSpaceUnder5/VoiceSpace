690426608e62959347ac98ee6fe3bb0a695a3987
honlee <kij753@naver.com>
2021-09-28 11:33:48 +0900

    [SW-142][added] screenShare component layout 완성
    - 버튼 이벤트들 아직 완성되지 않은 상태입니다.
    - screenShare 끄기 버튼은 제거 할까 생각하고 있습니다.
    - Draw 부분 지속적으로 개발할 예정입니다.

--- front/src/components/ScreenShare.tsx
+++ front/src/components/ScreenShare.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Menu, Dropdown} from 'antd';
+import {Menu, Dropdown, message, Switch, Button} from 'antd';
 import {DesktopOutlined} from '@ant-design/icons';
 import {Rnd} from 'react-rnd';
 import '../pages/spacePage/space.css';
+import {SwitchChangeEventHandler} from 'antd/lib/switch';
 
 interface ScreenViewerProps {
   stream: MediaStream;
@@ -14,7 +15,13 @@ interface ScreenShareData {
 }
 
 function ScreenViewer(props: ScreenViewerProps): JSX.Element {
+  // values
+  const headerHeight = 20;
+  const originVideoHeight = 120;
+  const originAspectRatio = 16 / 9;
+
   const videoRef = useRef<HTMLVideoElement>(null);
+  const rndRef = useRef<Rnd>(null);
   useEffect(() => {
     if (videoRef.current) videoRef.current.srcObject = props.stream;
   }, []);
@@ -23,13 +30,24 @@ function ScreenViewer(props: ScreenViewerProps): JSX.Element {
     .getVideoTracks()[0]
     .getSettings().aspectRatio;
 
-  const height = 90;
-  const width = aspectRatio ? 90 * aspectRatio : 160;
+  const drawToogleChagne: SwitchChangeEventHandler = () => {
+    // if (rndRef.current) {
+    //   console.log(rndRef.current.draggable);
+    // }
+  };
+  const height = originVideoHeight + headerHeight;
+  const width = aspectRatio
+    ? originVideoHeight * aspectRatio
+    : originVideoHeight * originAspectRatio;
   return (
     <div style={{position: 'absolute', left: 20, top: 20}}>
       <Rnd
-        lockAspectRatio={true}
-        style={{zIndex: 999, padding: 0, border: '2px solid white'}}
+        minWidth={width}
+        minHeight={height}
+        ref={rndRef}
+        lockAspectRatio={aspectRatio ? aspectRatio : originAspectRatio}
+        lockAspectRatioExtraHeight={headerHeight + 1}
+        style={{zIndex: 999, padding: 0, margin: 0, border: '2px solid white'}}
         default={{
           x: 0,
           y: 0,
@@ -37,17 +55,76 @@ function ScreenViewer(props: ScreenViewerProps): JSX.Element {
           height: height,
         }}
       >
+        <p
+          style={{
+            width: '100%',
+            height: `${headerHeight}px`,
+            margin: 0,
+            padding: 0,
+            background: 'white',
+            fontSize: `${headerHeight / 2}px`,
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div>Shared Screen</div>
+          <div
+            style={{
+              margin: 0,
+              paddingRight: `${headerHeight / 2}px`,
+              paddingLeft: `${headerHeight / 2}px`,
+            }}
+          >
+            <Switch
+              checkedChildren={'Stop'}
+              unCheckedChildren={'Draw'}
+              onChange={drawToogleChagne}
+              style={{
+                left: 0,
+                top: 0,
+                height: `${headerHeight - 1}px`,
+                marginRight: `${headerHeight / 4}px`,
+                padding: 0,
+              }}
+            ></Switch>
+            <Button
+              style={{
+                margin: 0,
+                padding: 0,
+                minWidth: `${headerHeight * 0.9}px`,
+                height: `${headerHeight * 0.9}px`,
+                width: `${headerHeight * 0.9}px`,
+                fontSize: `${headerHeight / 4}px`,
+              }}
+              shape="circle"
+            >
+              X
+            </Button>
+          </div>
+        </p>
         <video
           style={{
             top: 0,
             left: 0,
             width: '100%',
-            height: '100%',
+            height: `calc(100% - ${headerHeight}px)`,
           }}
           autoPlay={true}
           ref={videoRef}
           controls={false}
         ></video>
+        <canvas
+          onClick={() => {
+            console.log('canvas clicked!');
+          }}
+          style={{
+            top: '20px',
+            left: 0,
+            width: '100%',
+            height: `calc(100% - ${headerHeight}px)`,
+            position: 'absolute',
+          }}
+        ></canvas>
       </Rnd>
     </div>
   );
@@ -66,6 +143,16 @@ function ScreenShare(props: ScreenShareProps): JSX.Element {
     [],
   );
   const screenShareOnClick = async () => {
+    if (
+      screenShareDatas.find(data => {
+        return data.peerId === '';
+      })
+    ) {
+      message.info(
+        '이미 공유중인 화면이 존재합니다. 중지후 다시 선택해주세요!',
+      );
+      return;
+    }
     // eslint-disable-next-line
     const stream = await (navigator.mediaDevices as any).getDisplayMedia(); // 핸드폰일 경우 사용 불가.
     props.addVideoTrack(stream);
@@ -131,7 +218,7 @@ function ScreenShare(props: ScreenShareProps): JSX.Element {
       {screenShareDatas.map(screenShareData => {
         return (
           <ScreenViewer
-            key={screenShareData.stream.id}
+            key={screenShareData.peerId}
             stream={screenShareData.stream}
           ></ScreenViewer>
         );
