248a20f5fde6ce3698b6163ec01760622ba800ba
Hongkwon Lee <honlee@c8r5s14.42seoul.kr>
2021-10-21 12:51:43 +0900

    [bugfix][hotfix] spaceMain 에서 뒤로가기/나가기 버튼을 누른 후 다시입장시 로딩이 안되는 현상 해결

--- front/src/utils/pixiUtils/PixiCanvas.ts
+++ front/src/utils/pixiUtils/PixiCanvas.ts
@@ -15,17 +15,19 @@ export function pixiCanvasStart(
 ): void {
   SceneManager.changeCanvas(canvas);
   SceneManager.initialize(0xffffff);
-  ResourceManager.loadResourcesFrom(resourceUrls);
   ResourceManager.setOnErrorCallback(errorCB);
-  ResourceManager.setOnProgressCallback(progressCB);
-  ResourceManager.run(start);
-}
+  ResourceManager.setOnProgressCallback(progressCB); // 이미 로딩이 다 되어있을 경우 progressCB 이 한번도 실행되지 않습니다.
+  ResourceManager.loadResourcesFrom(resourceUrls);
 
-function start(): void {
-  const gameScene = new GameScene();
-  console.log(gameScene);
-  console.log('changeScene to GameScene');
-  SceneManager.changeScene(gameScene);
+  const start = (): void => {
+    progressCB(100); // 따라서 start 가 호출이 되면 어짜피 로딩이 다 되었다는 뜻이기 때문에 다시한번 progressCB 에 100을 넣어서 호출 하였습니다.
+    const gameScene = new GameScene();
+    console.log(gameScene);
+    console.log('changeScene to GameScene');
+    SceneManager.changeScene(gameScene);
+  };
+
+  ResourceManager.run(start);
 }
 
 export function pixiCanvasDestroy(): void {
