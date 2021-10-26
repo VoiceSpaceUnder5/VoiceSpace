4eb590ca842097fe63de47aa566658e1e4b7099d
Hongkwon Lee <honlee@c8r5s14.42seoul.kr>
2021-10-26 15:00:59 +0900

    [hotfix] worker 수정

---
+++ front/src/utils/pixiUtils/TickerWorker.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line
+export function tickerWorker() {
+  let intervalId;
+
+  addEventListener('message', event => {
+    if (event.data.run === true) {
+      intervalId = setInterval(() => {
+        // console.log(`SendMessage: `, event.data.run);
+        // console.log(event);
+        postMessage({message: 'run'});
+      }, 1000 / 60);
+    }
+
+    if (event.data.run === false) {
+      clearInterval(intervalId);
+    }
+  });
+}
