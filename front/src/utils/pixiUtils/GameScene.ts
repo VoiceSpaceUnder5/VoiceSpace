89a751a3255e8ad033f4660a24b76e093995086e
Likilee <gnsdlrl@daum.net>
2021-10-30 00:03:25 +0900

    [SW-273][added]뷰포트 컬링 test 추가

--- front/src/utils/pixiUtils/GameScene.ts
+++ front/src/utils/pixiUtils/GameScene.ts
@@ -6,10 +6,12 @@ import {World} from './World';
 import {GameData} from './GameData';
 import world1Json from './metaData/world1.json';
 import {createViewport, setViewportFollow} from './ViewportUtils';
+import {DisplayObjectWithCulling, Simple, SpatialHash} from 'pixi-cull';
 
 export class GameScene extends Container implements Scene {
   private viewport: Viewport;
   private world: World;
+  private cull: SpatialHash | Simple;
 
   constructor() {
     super();
@@ -35,6 +37,19 @@ export class GameScene extends Container implements Scene {
     world.addMyAvatar(player);
     setViewportFollow(viewport, player);
 
+    //viewport Culling 추가
+    this.cull = new Simple();
+    //updateObject 메소드를 수정해주어야 왼쪽을 볼 때도 정상적으로 AABB를 그린다..
+    this.cull.updateObject = (object: DisplayObjectWithCulling) => {
+      const box = object.getLocalBounds();
+      object.AABB = object.AABB || {x: 0, y: 0, width: 0, height: 0};
+      object.AABB.x = object.x + (box.x - object.pivot.x);
+      object.AABB.y = object.y + (box.y - object.pivot.y);
+      object.AABB.width = box.width; //* object.scale.x;
+      object.AABB.height = box.height; //* object.scale.y;
+    };
+    this.cull.addList(world.children);
+
     //Peer를 World에 추가하고 제거하는 기능
     GameData.addExistingPeers(world);
     GameData.addOnPeerCreatedHandler(world.addPeerAvatar.bind(world));
@@ -42,7 +57,13 @@ export class GameScene extends Container implements Scene {
   }
 
   public update(framesPassed: number): void {
+    //viewport Culling 추가
     this.world.update(framesPassed);
+    if (this.viewport.dirty || this.world.sortDirty) {
+      this.cull.cull(this.viewport.getVisibleBounds());
+      // console.log(this.cull.query(this.viewport.getVisibleBounds()));
+      this.viewport.dirty = false;
+    }
   }
 
   public resize(screenWidth: number, screenHeight: number): void {
